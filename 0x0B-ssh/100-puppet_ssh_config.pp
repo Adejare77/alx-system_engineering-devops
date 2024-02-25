5561f142478346a10aadfc3815828ce176879c1a
Adejare <adejare.arasheed@gmail.com>
2024-02-25 22:24:50 +0100

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,3 +5,16 @@ file { 'change_ssh_config':
   path    => '/etc/ssh/ssh_config',
   content => "PasswordAuthentication no\nIdentityFile ~/.ssh/school\n",
 }
+
+
+# The below would have work too, but not all puppet supports augeas
+# To install: puppet module install puppetlabs-augeas_core
+
+# augeas { 'change_ssh_config':
+#   lens    => 'Shellvars.lns',
+#   incl    => '/etc/ssh/ssh_config',
+#   changes => [
+#     'set IdentityFile ~/.ssh/school',
+#     'set PasswordAuthentication no'
+#   ]
+# }
