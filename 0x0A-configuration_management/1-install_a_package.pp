f7ddb9349637ed75eece6cb5de23d4ef4e98bd5e
Adejare <adejare.arasheed@gmail.com>
2024-02-25 21:24:22 +0100

    Configuration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,14 @@
 # Install a specific version of flask from pip3
 
-package { 'python3':
-  ensure   => '3.8.10',
-   name    => 'python3',
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3',
 }
 
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
   provider => 'pip3',
-  require  => Package['python3']
+  require  => Package['Werkzeug']
 }
