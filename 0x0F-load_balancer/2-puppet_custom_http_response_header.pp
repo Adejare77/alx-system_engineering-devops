661e493912f56751c48c00c1321a1b3b590bdd0d
Adejare <adejare.arasheed@gmail.com>
2024-03-04 22:37:59 +0100

    Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,28 +2,27 @@
 # header (on web-01 and web-02) with Puppet.
 
 exec { 'update_and_upgrade':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install_nginx'],
+  command => 'apt update && apt upgrade -y',
+  path    => '/usr/bin:/bin',
+  before  => Package['install_nginx']
 }
 
-
-exec {'install_nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  # before   => Exec['add_header'],
+package { 'install_nginx':
+  ensure   => 'installed',
+  name     => 'nginx',
+  provider => apt,
+  before   => Exec['custom_header'],
 }
 
-
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart_nginx'],
+exec { 'custom_header':
+  command => 'sed -i "16i\\        add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
+  path    => '/usr/bin:/bin',
+  require => Package['install_nginx'],
+  notify  => Exec['restart_nginx']
 }
 
 exec { 'restart_nginx':
   provider => shell,
-  command  => 'sudo service nginx restart',
+  command  => '/usr/sbin/service nginx restart',
 }
 
