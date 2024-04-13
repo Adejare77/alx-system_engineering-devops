787502c27cb757cb09c1ed7d552a5b9aa721784c
Adejare <adejare.arasheed@gmail.com>
2024-04-13 17:22:18 +0100

    Web Stack Debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# the extension phpp is wrong. find phpp in wp-settings.php and replace with php
+
 exec { 'replace_phpp':
   command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/usr/sbin']
