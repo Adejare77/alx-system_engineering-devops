e5a4059d1c3c305dafb0f95a2678b48120ecbc4c
Adejare <adejare.arasheed@gmail.com>
2024-04-13 17:29:09 +0100

    Web Stack Debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix 500 error. find phpp in wp-settings.php and replace with php
 
 exec { 'replace_phpp':
-  path    => ['/usr/bin', '/usr/sbin'],
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  provider => shell,
 }
